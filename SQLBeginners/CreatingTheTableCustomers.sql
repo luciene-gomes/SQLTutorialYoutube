dfaab75338493cd48792b9c3a2168792b9fed2e9
Luciene Gomes <91456181+luciene-gomes@users.noreply.github.com>
2022-12-28 12:36:45 +0000

    Add files via upload

---
+++ SQLBeginners/CreatingTheTableCustomers.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS CUSTOMERS
+
+CREATE TABLE CUSTOMERS
+(CustomerID				int,
+FirstName				varchar(50),
+LastName				varchar(50),
+FinalOrderPrice			int)
+
+INSERT INTO CUSTOMERS VALUES
+(101, 'Rafael','Silva', 100),
+(102, 'Maria', 'Ferreira', 200),
+(103, 'Carlos', 'Oliveira', 150),
+(104, 'Laura', 'Santos', 1000),
+(105, 'Laura', 'Santos', 589)
+
